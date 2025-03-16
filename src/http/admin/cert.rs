e14cb0b925752ab42095290f101c60cfb1249d91
_index <indexd@pm.me>
2025-03-16 15:14:07 +0100

    refactor cert.rs

--- src/http/admin/cert.rs
+++ src/http/admin/cert.rs
@@ -6,38 +6,14 @@ use esp_idf_svc::tls::{self, EspTls, X509};
 
 use crate::utils::nvs::{Certificate, WgConfig};
 
-// cannot use include_str because we need a \0 at the end
-const CA_CERT: &str = "-----BEGIN CERTIFICATE-----
-MIIEvjCCA6agAwIBAgIQBtjZBNVYQ0b2ii+nVCJ+xDANBgkqhkiG9w0BAQsFADBh
-MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
-d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD
-QTAeFw0yMTA0MTQwMDAwMDBaFw0zMTA0MTMyMzU5NTlaME8xCzAJBgNVBAYTAlVT
-MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxKTAnBgNVBAMTIERpZ2lDZXJ0IFRMUyBS
-U0EgU0hBMjU2IDIwMjAgQ0ExMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC
-AQEAwUuzZUdwvN1PWNvsnO3DZuUfMRNUrUpmRh8sCuxkB+Uu3Ny5CiDt3+PE0J6a
-qXodgojlEVbbHp9YwlHnLDQNLtKS4VbL8Xlfs7uHyiUDe5pSQWYQYE9XE0nw6Ddn
-g9/n00tnTCJRpt8OmRDtV1F0JuJ9x8piLhMbfyOIJVNvwTRYAIuE//i+p1hJInuW
-raKImxW8oHzf6VGo1bDtN+I2tIJLYrVJmuzHZ9bjPvXj1hJeRPG/cUJ9WIQDgLGB
-Afr5yjK7tI4nhyfFK3TUqNaX3sNk+crOU6JWvHgXjkkDKa77SU+kFbnO8lwZV21r
-eacroicgE7XQPUDTITAHk+qZ9QIDAQABo4IBgjCCAX4wEgYDVR0TAQH/BAgwBgEB
-/wIBADAdBgNVHQ4EFgQUt2ui6qiqhIx56rTaD5iyxZV2ufQwHwYDVR0jBBgwFoAU
-A95QNVbRTLtm8KPiGxvDl7I90VUwDgYDVR0PAQH/BAQDAgGGMB0GA1UdJQQWMBQG
-CCsGAQUFBwMBBggrBgEFBQcDAjB2BggrBgEFBQcBAQRqMGgwJAYIKwYBBQUHMAGG
-GGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBABggrBgEFBQcwAoY0aHR0cDovL2Nh
-Y2VydHMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0R2xvYmFsUm9vdENBLmNydDBCBgNV
-HR8EOzA5MDegNaAzhjFodHRwOi8vY3JsMy5kaWdpY2VydC5jb20vRGlnaUNlcnRH
-bG9iYWxSb290Q0EuY3JsMD0GA1UdIAQ2MDQwCwYJYIZIAYb9bAIBMAcGBWeBDAEB
-MAgGBmeBDAECATAIBgZngQwBAgIwCAYGZ4EMAQIDMA0GCSqGSIb3DQEBCwUAA4IB
-AQCAMs5eC91uWg0Kr+HWhMvAjvqFcO3aXbMM9yt1QP6FCvrzMXi3cEsaiVi6gL3z
-ax3pfs8LulicWdSQ0/1s/dCYbbdxglvPbQtaCdB73sRD2Cqk3p5BJl+7j5nL3a7h
-qG+fh/50tx8bIKuxT8b1Z11dmzzp/2n3YWzW2fP9NsarA4h20ksudYbj/NhVfSbC
-EXffPgK2fPOre3qGNm+499iTcc+G33Mw+nur7SpZyEKEOxEXGlLzyQ4UfaJbcme6
-ce1XR2bFuAJKZTRei9AqPCCcUZlM51Ke92sRKw2Sfh3oius2FkOH6ipjv3U/697E
-A7sKPPcw7+uvTPyLNhBzPvOk
------END CERTIFICATE-----\0";
+const CA_CERT: &str = include_str!("ca_cert.pem");
+const HOSTNAME: &str = "charizhard.duckdns.org";
 
 pub fn fetch_config(nvs: Arc<Mutex<EspNvs<NvsDefault>>>) -> anyhow::Result<WgConfig> {
     let mut tls = EspTls::new()?;
+
+    // need to null-terminate the cert
+    let ca_cert = [CA_CERT.as_bytes(), &[0]].concat();
 
     let client_cert = Certificate::get_config(Arc::clone(&nvs))?;
 
@@ -46,16 +22,16 @@ pub fn fetch_config(nvs: Arc<Mutex<EspNvs<NvsDefault>>>) -> anyhow::Result<WgCon
         return Err(anyhow::anyhow!("No certificate to request wireguard configuration with!"));
     }
 
-    tls.connect("charizhard.duckdns.org", 443, &tls::Config {
-        common_name: Some("charizhard.duckdns.org"),
-        ca_cert: Some(X509::pem(CStr::from_bytes_with_nul(CA_CERT.as_bytes())?)),
+    tls.connect(HOSTNAME, 443, &tls::Config {
+        common_name: Some(HOSTNAME),
+        ca_cert: Some(X509::pem(CStr::from_bytes_with_nul(&ca_cert)?)),
         client_cert: Some(X509::pem(CStr::from_bytes_with_nul(client_cert.cert.as_bytes())?)),
         client_key: Some(X509::pem(CStr::from_bytes_with_nul(client_cert.privkey.as_bytes())?)),
         alpn_protos: Some(&["http/1.1"]),
         ..Default::default()
     })?;
 
-    let request = "GET /get-config HTTP/1.1\r\nHost: charizhard.duckdns.org\r\nConnection: close\r\n\r\n";
+    let request = format!("GET /get-config HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n", HOSTNAME);
 
     tls.write_all(request.as_bytes())?;
 
